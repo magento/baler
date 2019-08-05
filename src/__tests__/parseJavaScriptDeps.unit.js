dc631623737aa7dfdcc663571e74c36d660f4920
Andrew Levine <DrewML@users.noreply.github.com>
2019-08-05 18:40:32 -0500

    Add failing test case for parseJavaScriptDeps

--- src/__tests__/parseJavaScriptDeps.unit.js
+++ src/__tests__/parseJavaScriptDeps.unit.js
@@ -81,3 +81,34 @@ test('Reports incomplete analsys when a dep in define is not a string literal', 
     expect(incompleteAnalysis).toBe(true);
     expect(deps).toHaveLength(0);
 });
+
+test('Can handle the UMD dance from knockout-es5.js in core', () => {
+    const input = `
+        function prepareExports() {
+            if (typeof exports === 'object' && typeof module === 'object') {
+            // Node.js case - load KO and WeakMap modules synchronously
+            ko = require('knockout');
+            var WM = require('../lib/weakmap');
+            attachToKo(ko);
+            weakMapFactory = function() { return new WM(); };
+            module.exports = ko;
+            } else if (typeof define === 'function' && define.amd) {
+            define(['knockout'], function(koModule) {
+                ko = koModule;
+                attachToKo(koModule);
+                weakMapFactory = function() { return new global.WeakMap(); };
+                return koModule;
+            });
+            } else if ('ko' in global) {
+            // Non-module case - attach to the global instance, and assume a global WeakMap constructor
+            ko = global.ko;
+            attachToKo(global.ko);
+            weakMapFactory = function() { return new global.WeakMap(); };
+            }
+        }
+    `;
+
+    const { deps, incompleteAnalysis } = parseJavaScriptDeps(input);
+    expect(deps).toEqual(['knockout']);
+    expect(incompleteAnalysis).toBe(false);
+});
