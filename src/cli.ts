b4c4503318bf53c791b1bc997cc967837bcff273
Andrew Levine <DrewML@users.noreply.github.com>
2019-08-28 16:36:03 -0500

    Remove hardcoding of Magento/luma in cli.ts

--- src/cli.ts
+++ src/cli.ts
@@ -10,42 +10,32 @@ import { StoreData } from './types';
 export async function run(cwd: string) {
     const magentoRoot = await findMagentoRoot(cwd);
     if (!magentoRoot) {
-        log.error('Could not find a Magento 2 installation');
-        process.exit(1);
-        // Shutting up TypeScript, which doesn't know that
-        // process.exit halts execution
-        throw void 0;
+        throw new Error(`Could not find a Magento 2 installation from ${cwd}`);
     }
 
     const store = await collectStoreData(magentoRoot);
-    exitWithMessageIfNoDeployedThemes(store);
+    const themesToOptimize = getSupportedAndDeployedThemeIDs(store);
 
-    const results = await optimizeThemes(magentoRoot, store, ['Magento/luma']);
-    console.log(JSON.stringify(results, null, 2));
+    if (!themesToOptimize.length) {
+        throw new Error(
+            'No eligible themes were found to be optimized. For a theme ' +
+                'to be optimized, it must:\n\n' +
+                '  - Be for the "frontend" area\n' +
+                '  - Not be "Magento/blank"\n' +
+                '  - Be deployed already with bin/magento setup:static-content:deploy\n',
+        );
+    }
+
+    const results = await optimizeThemes(magentoRoot, store, themesToOptimize);
+    const { graph, ...rest } = results[0].result!;
+    console.log(JSON.stringify(rest, null, 2));
 }
 
-/**
- * @summary If no deployed themes exist in pub/static,
- *          log a descriptive warning and exit the process
- *          with an error
- */
-function exitWithMessageIfNoDeployedThemes(store: StoreData): void {
-    const { deployedThemes, components } = store;
-    const allThemeNames = Object.values(components.themes)
+function getSupportedAndDeployedThemeIDs(store: StoreData): string[] {
+    const { components, deployedThemes } = store;
+
+    return Object.values(components.themes)
         .filter(t => t.area === 'frontend' && t.themeID !== 'Magento/blank')
+        .filter(t => deployedThemes.includes(t.themeID))
         .map(t => t.themeID);
-    const eligibleDeployedThemes = allThemeNames.filter(id =>
-        deployedThemes.includes(id),
-    );
-
-    if (eligibleDeployedThemes.length) return;
-
-    log.error(
-        "No deployed frontend theme(s) were found in your store's static directory, " +
-            'but the following are installed in your store:\n' +
-            allThemeNames.map(n => `  - ${n}`).join('\n'),
-        '\n Try running "bin/magento setup:static-content:deploy", ' +
-            'then try running "baler" again.',
-    );
-    process.exit(1);
 }
