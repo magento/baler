c7ff6293d248231873a7cb18fcb6274a9ae3157a
Andrew Levine <DrewML@users.noreply.github.com>
2020-03-03 14:02:04 -0600

    Add TODO comment about exclusion of js-translation.json

--- src/excludedFiles.ts
+++ src/excludedFiles.ts
@@ -3,6 +3,12 @@
  * See COPYING.txt for license details.
  */
 
-const EXCLUDED_FILES: ReadonlyArray<string> = ['text!js-translation.json'];
+const EXCLUDED_FILES: ReadonlyArray<string> = [
+    // TODO: translation files are excluded as a work-around
+    // for a bug. Implementing the "thorough" solution from
+    // the following issue will speed up storefronts
+    // https://github.com/magento/baler/issues/47#issuecomment-582580154
+    'text!js-translation.json',
+];
 
 export { EXCLUDED_FILES };
